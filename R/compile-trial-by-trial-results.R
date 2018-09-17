4fa6eb656a204a071a5b728d19e58c21098eb70f
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-17 10:23:07 +0100

    Adding compile_trial_by_trial_results()

---
+++ R/compile-trial-by-trial-results.R
@@ -0,0 +1,51 @@
+#' Compile trial-by-trial results
+#'
+#' Compiles a data frame of trial-by-trial results for each participant.
+#' This function is intended to be called once data have been collected
+#' for several participants; it allows the researcher to inspect
+#' test results on a trial-by-trial level.
+#' @param in_dir Results directory to process.
+#' @param label Label that the test's results were saved under (e.g. "MDT").
+#' @param combine Whether to combine results into one big data frame,
+#' or instead to return a list of data frames, one for each participant.
+#' @return A data frame, or list of data frames, of trial-by-trial results.
+#' @export
+compile_trial_by_trial_results <- function(in_dir = "output/results", label,
+                                           combine = TRUE) {
+  assertthat::assert_that(is.character(in_dir), assertthat::is.scalar(in_dir),
+                          is.logical(combine), assertthat::is.scalar(combine))
+  if (!dir.exists(in_dir))
+    stop("input directory '", in_dir, "' could not be found")
+  files <- list.files(in_dir, pattern = "\\.rds",
+                      ignore.case = TRUE, full.names = TRUE)
+  n <- list(rds = length(files),
+            results = 0L,
+            test = 0L)
+  res <- list()
+  for (i in seq_along(files)) {
+    file <- files[i]
+    x <- readRDS(file)
+    if (is(x, "results")) {
+      n$results <- n$results + 1L
+      x <- as.list(x)
+      if (!is.null(x[[label]])) {
+        if (sum(names(x) == label) > 1L)
+          stop("found a results file ('", file, "')",
+               " with multiple outputs with label '", label,
+               "', not sure what to do here. You might have to process ",
+               "this directory manually with readRDS().")
+        n$test <- n$test + 1L
+        df <- attr(x[[label]]$ability, "metadata")$results
+        if (!is.data.frame(df)) stop("malformed ", label, " results for file ", file)
+        p_id <- x$session$p_id
+        df <- cbind(p_id = p_id, test = label, file = file, df)
+        res[[length(res) + 1L]] <- df
+      }
+    }
+  }
+  if (combine) res <- do.call(rbind, res)
+  message(sprintf(paste0("processed %i RDS files, %i of which were psychTestR results files, ",
+                         "%i of which contained %s results"),
+                  n$rds, n$results, n$test, label))
+  res
+}
